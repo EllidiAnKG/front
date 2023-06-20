510f48c3a77a5e32d144e1ae6715ec8ceb776924
EllidiAnKG <112854201+EllidiAnKG@users.noreply.github.com>
2023-06-20 23:47:51 +0500

    Add files via upload

---
+++ front/addingTeams.js
@@ -0,0 +1,30 @@
+const teams = document.querySelector('.flexBlock')
+const url = 'http://localhost:3000/profile/team';
+const options ={
+    method: 'GET'
+}
+const newTeam = (team) => {
+    const teamsDiv = document.createElement('div')
+    teamsDiv.classList.add('moreNewBlock')
+    console.log(team.game)
+    teamsDiv.innerHTML =
+        `<p>Игра: ${team.game}</p>
+        <p>Название команды: ${team.teamName}</p>
+        <p>Капитан: ${team.captain }</p>
+        <p>Возраст: <br>16+</br></p>
+        <p>Кол-во:<br>1/18</br></p>
+        <p><br>New!</br></p>
+        `
+
+    teams.append(teamsDiv)
+}
+const teamsPost = async () => {
+    try {
+        const teams = await fetch(url, options)
+        const teamsJson = await teams.json()
+        inPrint = teamsJson.map(teams => newTeam(teams))
+    } catch (error) {
+        console.error()
+    }
+}
+teamsPost();
